bb70aef89d97bad65729fcef9ba5ef6c8466af64
Maxim Usikov <mu.usikov@gmail.com>
2018-10-11 13:42:33 +0300

    feat: add bot entry and config env.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './env.js';
+import Discord from 'discord.js';
+
+const { DISCORD_BOT_TOKEN } = process.env;
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Discord client is READY!');
+});
+
+client.login(DISCORD_BOT_TOKEN);
