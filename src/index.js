8125014c92e013f07c7e5bab69222d2942e10161
Maxim Usikov <mu.usikov@gmail.com>
2018-10-11 15:03:14 +0300

    fix: lint.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './env.js';
+import './env';
 import Discord from 'discord.js';
 
 const { DISCORD_BOT_TOKEN } = process.env;
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('Discord client is READY!');
+  console.log('Discord client is READY!');
 });
 
 client.login(DISCORD_BOT_TOKEN);
